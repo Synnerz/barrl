414d4a3244565ff419f6b59573f02004ace5047a
Doc <65479309+DocilElm@users.noreply.github.com>
2025-10-19 20:51:48 -0700

    add support for mc v1.21.6

--- src/main/java/com/github/synnerz/barrl/mixin/WorldRendererMixin.java
+++ src/main/java/com/github/synnerz/barrl/mixin/WorldRendererMixin.java
@@ -3,12 +3,14 @@ package com.github.synnerz.barrl.mixin;
 import com.github.synnerz.barrl.Context;
 import com.github.synnerz.barrl.events.WorldRenderEvent;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.mojang.blaze3d.buffers.GpuBufferSlice;
 import net.minecraft.client.render.*;
 import net.minecraft.client.util.Handle;
 import net.minecraft.client.util.ObjectAllocator;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.profiler.Profiler;
 import org.joml.Matrix4f;
+import org.joml.Vector4f;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -22,7 +24,7 @@ public class WorldRendererMixin {
     private Context ctx = new Context();
 
     @Inject(method = "render", at = @At("HEAD"))
-    private void preRender(ObjectAllocator allocator, RenderTickCounter tickCounter, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, Matrix4f positionMatrix, Matrix4f projectionMatrix, CallbackInfo ci) {
+    private void preRender(ObjectAllocator allocator, RenderTickCounter tickCounter, boolean renderBlockOutline, Camera camera, Matrix4f positionMatrix, Matrix4f projectionMatrix, GpuBufferSlice fog, Vector4f fogColor, boolean shouldRenderSky, CallbackInfo ci) {
         // Setup the context variable
         ctx.setup(bufferBuilders.getEntityVertexConsumers(), tickCounter, camera);
         Context.Companion.setImmediate(ctx);
@@ -30,7 +32,7 @@ public class WorldRendererMixin {
     }
 
     @Inject(method = "method_62214", at = @At("RETURN"))
-    private void postRender(Fog fog, RenderTickCounter renderTickCounter, Camera camera, Profiler profiler, Matrix4f matrix4f, Matrix4f matrix4f2, Handle handle, Handle handle2, boolean bl, Frustum frustum, Handle handle3, Handle handle4, CallbackInfo ci) {
+    private void postRender(GpuBufferSlice gpuBufferSlice, RenderTickCounter renderTickCounter, Camera camera, Profiler profiler, Matrix4f matrix4f, Handle handle, Handle handle2, boolean bl, Frustum frustum, Handle handle3, Handle handle4, CallbackInfo ci) {
         WorldRenderEvent.LAST.invoker().trigger(ctx);
     }
 
