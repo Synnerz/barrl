70c8437217b69a0acea3be89b73c5adba3a88ea0
Doc <65479309+DocilElm@users.noreply.github.com>
2025-10-19 23:39:59 -0700

    make client mod & add jitpack

--- src/main/kotlin/com/github/synnerz/barrl/Barrl.kt
+++ src/main/kotlin/com/github/synnerz/barrl/Barrl.kt
@@ -1,12 +1,12 @@
 package com.github.synnerz.barrl
 
-import net.fabricmc.api.ModInitializer
+import net.fabricmc.api.ClientModInitializer
 import org.slf4j.LoggerFactory
 
-object Barrl : ModInitializer {
+object Barrl : ClientModInitializer {
     private val logger = LoggerFactory.getLogger("barrl")
 
-	override fun onInitialize() {
+	override fun onInitializeClient() {
 		logger.info("Initialized Synnerz/Barrl library")
 	}
 }
