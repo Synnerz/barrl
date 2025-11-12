e037ae316b94a8df5839f1183745d173634df8b6
Ch1ck3nNeedsRNG <chick_is_bored>
2025-11-12 15:21:33 -0500

    silly doc

--- src/main/kotlin/com/github/synnerz/barrl/utils/Render3D.kt
+++ src/main/kotlin/com/github/synnerz/barrl/utils/Render3D.kt
@@ -23,8 +23,8 @@ object Render3D {
      * @param ctx The Context instance
      * @param shape The VoxelShape instance to render
      * @param ox The x offset
-     * @param oy The Y offset
-     * @param oz The Z offset
+     * @param oy The y offset
+     * @param oz The z offset
      * @param color The color
      * @param phase Whether to render through walls or not (`false` = no)
      */
@@ -135,8 +135,8 @@ object Render3D {
      * @param ctx The Context instance
      * @param shape The VoxelShape instance to render
      * @param ox The x offset
-     * @param oy The Y offset
-     * @param oz The Z offset
+     * @param oy The y offset
+     * @param oz The z offset
      * @param color The color
      * @param phase Whether to render through walls or not (`false` = no)
      */
